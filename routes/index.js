65e08f28f6a007ae36aafba848e3dbd5607cd16f
kkerchaoui <khelil.k@gmail.com>
2019-06-30 17:09:33 +0200

    changing index to home page

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { page: 'Home',menuId:'home' });
-});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { page: 'Home',menuId:'home' });
+  res.render('home', { page: 'Home',menuId:'home' });
 });
 
 /* GET home page. */
