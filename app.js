90583aaba897e2a4bb2786f19629b5ce5f5494aa
kkerchaoui <khelil.k@gmail.com>
2019-07-18 16:22:48 +0200

    beautify

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,12 +30,12 @@ app.use('/contact', contactRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in developments
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/contact.js
+++ routes/contact.js
@@ -4,51 +4,53 @@ var nodeMailer = require('nodemailer');
 const creds = require('../config/config');
 const request = require('request');
 
-router.post('/', function(req, res) {
-    if(req.body.captcha === undefined || req.body.captcha === '' || req.body.captcha === null)
-    {
-      return res.json({responseError : "Please select captcha first"});
+router.post('/', function (req, res) {
+  if (req.body.captcha === undefined || req.body.captcha === '' || req.body.captcha === null) {
+    return res.json({
+      responseError: "Please select captcha first"
+    });
+  }
+
+  const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + creds.SECRET_KEY + "&response=" + req.body.captcha;
+
+  request(verificationURL, function (error, response, body) {
+    body = JSON.parse(body);
+
+    if (body.success !== undefined && !body.success) {
+      return res.json({
+        responseError: "Failed captcha verification"
+      });
     }
 
-    const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + creds.SECRET_KEY + "&response=" + req.body.captcha ;
-
-    request(verificationURL,function(error,response,body) {
-      body = JSON.parse(body);
-
-      if(body.success !== undefined && !body.success) {
-        return res.json({responseError : "Failed captcha verification"});
+    let mailOpts, smtpTrans;
+    smtpTrans = nodeMailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: creds.USER,
+        pass: creds.PASS
       }
+    });
+    mailOpts = {
+      from: req.body.name,
+      to: creds.USER,
+      subject: 'Site - Nouveau message de ' + req.body.name,
+      text: `Nom: ${req.body.name}\nMail: ${req.body.email}\nTel: ${req.body.tel} \n \n ${req.body.content}`
+    };
 
-      let mailOpts, smtpTrans;
-      smtpTrans = nodeMailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-          user: creds.USER,
-          pass: creds.PASS
-        }
-      });
-      mailOpts = {
-        from: req.body.name,
-        to: creds.USER,
-        subject: 'Site - Nouveau message de '+ req.body.name,
-        text: `Nom: ${req.body.name}\nMail: ${req.body.email}\nTel: ${req.body.tel} \n \n ${req.body.content}`
-      };
-    
-      smtpTrans.sendMail(mailOpts, function (error) {
-        if (error) {
-          res.json({
-            responseError: 'Oops something went wrong'
-          })
-        }
-        else {
-          res.json({
-            responseSuccess: 'Thanks for your message I\'ll get back to you shortly !'
-          })
-        }
-      });
+    smtpTrans.sendMail(mailOpts, function (error) {
+      if (error) {
+        res.json({
+          responseError: 'Oops something went wrong'
+        })
+      } else {
+        res.json({
+          responseSuccess: 'Thanks for your message I\'ll get back to you shortly !'
+        })
+      }
     });
   });
+});
 
 module.exports = router;
