c7646c55ebdaa37065d974860af2d6883d023bda
kkerchaoui <khelil.k@gmail.com>
2019-07-20 18:20:37 +0200

    comments

--- config/config_example.js
+++ config/config_example.js
@@ -2,5 +2,6 @@ module.exports={
     USER: 'john.doe@gmail.com',
     PASS: '*j0hnd03*',
     SECRET_KEY: '000D0030000303001010002023'
-    // Recaptcha SK
+    // Get a Recaptcha SK from google 
+    // Rename config_example => config
 }
