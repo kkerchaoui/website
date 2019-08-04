0191a621757992151db20ae9f4ad4f827208ad08
kkerchaoui <khelil.k@gmail.com>
2019-08-04 16:42:54 +0200

    fixing typos

--- config/config_example.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    USER: 'john.doe@gmail.com',
-    PASS: '*j0hnd03*',
-    SECRET_KEY: '000D0030000303001010002023'
-    // Modify USER & PASS corresponding to your mail acount
-    // Get a Recaptcha SK from google 
-    // Rename config_example => config
-}
